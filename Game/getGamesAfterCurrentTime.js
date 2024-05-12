312726cb056892d69b3317b7502c7ead3df4564b
Divyansh Acharya <56694553+DA-11@users.noreply.github.com>
2024-05-12 21:16:31 +0530

    Update getGamesAfterCurrentTime.js
    Updated Find Game After Current Time API

--- Game/getGamesAfterCurrentTime.js
+++ Game/getGamesAfterCurrentTime.js
@@ -1,11 +1,21 @@
 const asyncHandler = require("express-async-handler");
 const Game = require("../models/gameModel");
 const getGamesAfterCurrentTime =  asyncHandler(async function(req,res){
-    
+
+    console.log("Find Game after c time Api Hit");
     const turfName =  req.query.turfName;
+    console.log(`turfName = ${turfName}`);
     const date =  req.query.date; 
+    console.log(`date = ${date}`);
+
     const regex = `.*${turfName}.*`
-    const games = await Game.find({fromTime:{$gt:new Date(date)}});
+    let games;
+
+    if(turfName === '00'){
+        games = await Game.find({fromTime:{$gt:new Date(date)}});
+    } else {
+        games = await Game.find({turfName : {$regex : regex},fromTime:{$gt:new Date(date)}});
+    }
 
     if(!games){
         res.status(400);
